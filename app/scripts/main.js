00e6fff8c3bf512a837bdefcb93cbf2a6c54c9f3
jntakpe <jntakpe@gmail.com>
2016-12-15 18:09:43 +0100

    Lien vers portfolio depuis parralax

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+$(function () {
+  'use strict';
+
+  $('.portfolio-box-v1').find('.portfolio-box-v1-in a').click(function () {
+    const imgTitle = $(this).data('title');
+    $('.cbp-l-grid-gallery').find('.cbp-item a[data-title="' + imgTitle + '"]').trigger('click');
+  });
+});
