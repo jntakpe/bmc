d35f6639d909db7fefcd5a9eb228a0977fc84ee5
jntakpe <jntakpe@gmail.com>
2019-12-26 11:55:25 +0100

    Upload site

--- app/scripts/main.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  'use strict';
-
-  $('.portfolio-box-v1').find('.portfolio-box-v1-in a').click(function () {
-    const imgTitle = $(this).data('title');
-    $('.cbp-l-grid-gallery').find('.cbp-item a[data-title="' + imgTitle + '"]').trigger('click');
-  });
-});
